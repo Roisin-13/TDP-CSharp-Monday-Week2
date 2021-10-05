1db254ec7f3c50e7ba8a9df066cb9df1fe197cc2
Roisin-13 <roisin.wheeler@gmail.com>
2021-10-05 18:45:23 +0100

    lists and generic excercises

---
+++ ListsandGenerics/ListsandGenerics/CustomArrayList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListsandGenerics
+{
+    class CustomArrayList
+    {
+        private int[] arr;
+        public CustomArrayList(int[] values)
+        {
+            this.arr = values;
+        }
+        public void Add(int i)
+        {
+            //create new array with 1 more space for new value, copy over old values
+                int[] newArr = new int[arr.Length + 1];
+                arr.CopyTo(newArr, 0);
+             //add new value to end of array
+                newArr[newArr.Length - 1] = i;
+                arr = newArr;
+        }
+        public void PrintValues()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+            }
+        }
+        // Pop will remove the last element from the list and return it       
+        // - remember to make your array smaller after removing the element
+         public int Pop()
+        {
+            int item = arr[arr.Length - 1];
+
+            int[] newArr = new int[arr.Length - 1];
+            for (int i = 0; i < arr.Length -1; i++)
+            {
+                newArr[i] = arr[i];
+            }
+            //arr.CopyTo(newArr, 0);
+            arr = newArr;
+            return item;
+           //throw new NotImplementedException();
+        }
+
+
+
+}
+}
---
+++ ListsandGenerics/ListsandGenerics/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ListsandGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //==========Array============//
+            int[] arr = { 1, 2, 3, 4 };
+            //==========ArrayList created============//
+            CustomArrayList arrList = new CustomArrayList(arr);
+            //==========ArrayList TESTING============//
+            Console.WriteLine("printing values");
+            arrList.PrintValues();
+            Console.WriteLine();
+            arrList.Add(7);
+            Console.WriteLine("printing values after adding");
+            arrList.PrintValues();
+            Console.WriteLine();
+            Console.WriteLine("popped value and after");
+            Console.WriteLine(arrList.Pop());
+            arrList.PrintValues();
+        }
+    }
+}
